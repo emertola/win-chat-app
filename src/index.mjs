14e3c4e245ba8969324f5103094ab60da252dd51
emertola <emertola@gmail.com>
2024-02-21 09:15:48 +0800

    fix: remove the deprecated warning for express-session resave and saveUninitialized

--- src/index.mjs
+++ src/index.mjs
@@ -10,7 +10,7 @@ const PORT = 3001;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(session({ secret: 'session123' }));
+app.use(session({ secret: 'session123', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/api/v1', routes);
